c8c443831d43d910e482495dbb25a0c6baf8c1aa
5-Takubo-23 <j-takubo@g.magokorokaigo.com>
2022-10-03 21:44:22 +0900

    add check_answer func

--- quiz/models.py
+++ quiz/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class User(models.Model):
     user_id = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
 
 class Quiz(models.Model):
     q_order = models.IntegerField()
